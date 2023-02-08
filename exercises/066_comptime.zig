da2ff35575fef94159cbe3b4e9df3a337c7dfb2b
MetaSky <creator_meta_sky@163.com>
2023-02-08 21:49:18 +0800

    finish comptime.

--- exercises/066_comptime.zig
+++ exercises/066_comptime.zig
@@ -61,8 +61,8 @@ pub fn main() void {
     // values. Therefore, it follows that we just specify numeric
     // types with specific sizes. The comptime numbers will be
     // coerced (if they'll fit!) into your chosen runtime types.
-    var var_int = 12345;
-    var var_float = 987.654;
+    var var_int: u32 = 12345;
+    var var_float: f32 = 987.654;
 
     // We can change what is stored at the areas set aside for
     // "var_int" and "var_float" in the running compiled program.
@@ -80,4 +80,10 @@ pub fn main() void {
         @TypeOf(var_int),
         @TypeOf(var_float),
     });
+
+    var index: u32 = 10; // must known the size
+    print("the index = {}", .{index});
+
+    index = 20;
+    print("the index = {}", .{index});
 }
